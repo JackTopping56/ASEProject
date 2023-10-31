49ef091c00f40320f43866b7d881890b75947aea
Jack Topping <J.Topping2273@student.leedsbeckett.ac.uk>
2023-10-31 10:24:48 +0000

    XML Documentation - Change Fill Mode Command

--- ASEProject/ChangeFillModeCommand.cs
+++ ASEProject/ChangeFillModeCommand.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace ASEProject
 {
+    /// <summary>
+    /// Represents a command that changes the fill mode of a command list.
+    /// </summary>
     public class ChangeFillModeCommand : ICommand
     {
+        /// <summary>
+        /// Executes the command to change the fill mode of a specified command list.
+        /// </summary>
+        /// <param name="commandList">The command list to modify.</param>
+        /// <param name="parameters">An array of parameters specifying the new fill mode.</param>
         public void Execute(CommandList commandList, string[] parameters)
         {
             if (parameters.Length >= 2)
