bd82185b633d5526608c89e9b18a5523e099ac2b
Jack Topping <J.Topping2273@student.leedsbeckett.ac.uk>
2023-10-31 10:27:14 +0000

    XML Documenation - ChangePenColorCommand

--- ASEProject/ChangePenColorCommand.cs
+++ ASEProject/ChangePenColorCommand.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 
 namespace ASEProject
 {
-
+    /// <summary>
+    /// Represents a command that changes the pen color in a drawing application.
+    /// </summary>
     public class ChangePenColorCommand : ICommand
     {
+        /// <summary>
+        /// Executes the command to change the pen color.
+        /// </summary>
+        /// <param name="commandList">The command list to which this command belongs.</param>
+        /// <param name="parameters">An array of parameters that provide information about the new pen color.</param>
         public void Execute(CommandList commandList, string[] parameters)
         {
             if (parameters.Length >= 2)
