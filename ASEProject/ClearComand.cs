2ec89d0519f8bffa200ea0c91b9ebf8d48f51801
Jack Topping <J.Topping2273@student.leedsbeckett.ac.uk>
2023-10-31 10:28:04 +0000

    XML Documentation - ClearCommand

--- ASEProject/ClearComand.cs
+++ ASEProject/ClearComand.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace ASEProject
 {
+    /// <summary>
+    /// Represents a command that clears the drawing in a drawing application.
+    /// </summary>
     public class ClearCommand : ICommand
     {
+        /// <summary>
+        /// Executes the command to clear the drawing.
+        /// </summary>
+        /// <param name="commandList">The command list to which this command belongs.</param>
+        /// <param name="parameters">An array of parameters (not used in this command).</param>
         public void Execute(CommandList commandList, string[] parameters)
         {
             commandList.Clear();
