d49deb6f394b76701dd77b7bdd09fb8f0ea1ec49
Jack Topping <J.Topping2273@student.leedsbeckett.ac.uk>
2023-12-27 16:56:23 +0000

    Method XML Docs

--- ASEProject/Method.cs
+++ ASEProject/Method.cs
@@ -6,12 +6,35 @@ using System.Threading.Tasks;
 
 namespace ASEProject
 {
+    /// <summary>
+    /// Represents a method in the drawing application. 
+    /// A method is defined with a name, a list of parameters, and a list of commands to execute.
+    /// </summary>
     public class Method
     {
+        /// <summary>
+        /// Gets or sets the name of the method.
+        /// </summary>
+        /// <value>The name of the method.</value>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the list of parameters used by the method. These parameters can be used within the commands of the method.
+        /// </summary>
+        /// <value>A list of parameter names.</value>
         public List<string> Parameters { get; set; }
+
+
+        /// <summary>
+        /// Gets the list of commands that the method will execute. These commands are executed in the order they appear in the list.
+        /// </summary>
+        /// <value>A list of drawing commands.</value>
         public List<string> Commands { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Method"/> class with the specified name.
+        /// </summary>
+        /// <param name="name">The name of the method.</param>
         public Method(string name)
         {
             Name = name;
